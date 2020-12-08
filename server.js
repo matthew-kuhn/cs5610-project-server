86c2caefeda26f1591c53ba29feb409ffd05d31f
jack6177733651 <40873770+jack6177733651@users.noreply.github.com>
2020-12-07 22:28:16 -0500

    Changed cors to allow origin from heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_ADMIN_NAME}:${process.env.DB_PA
     { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://tranquil-basin-87439.herokuapp.com',
     credentials: true,
 }))
 
