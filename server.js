ea67dcc2f36145b1bafc1a9ef3166bf017f2ebdf
jack6177733651 <40873770+jack6177733651@users.noreply.github.com>
2020-12-07 22:52:14 -0500

    Trying to fix session on heroku

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const app = express()
 const cors = require('cors')
 const session = require('express-session');
 
+app.set('trust proxy', 1)
 app.use(session({
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
-    secret: 'adopifjqeporihgepoih349ru834tgihej'
+    secret: 'adopifjqeporihgepoih349ru834tgihej',
+    cookie: { secure: true }
 }));
 
 const bodyParser = require('body-parser');
