686fb5bfbdc58b76a7318027f4ff955555e884b5
Matthew Kuhn <matthewkuhn55@gmail.com>
2020-12-09 14:22:40 -0500

    fix server session persistence

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const session = require('express-session');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
-app.use(session({
+const mongoose = require("mongoose");
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_ADMIN_NAME}:${process.env.DB_PASSWORD}@cluster0.ecgcm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+
+app.use(
+  session({
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
-    secret: 'adopifjqeporihgepoih349ru834tgihej'
-}));
+    secret: "adopifjqeporihgepoih349ru834tgihej",
+    store: new MongoStore({ mongooseConnection: db }),
+  })
+);
 
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://${process.env.DB_ADMIN_NAME}:${process.env.DB_PASSWORD}@cluster0.ecgcm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true })
-
-app.use(cors({
-    origin: 'https://tranquil-basin-87439.herokuapp.com',
+app.use(
+  cors({
+    origin: "https://tranquil-basin-87439.herokuapp.com",
     credentials: true,
-}))
+  })
+);
 
-require('./controllers/users.controller.server')(app)
-require('./controllers/reviews.controller.server')(app)
+require("./controllers/users.controller.server")(app);
+require("./controllers/reviews.controller.server")(app);
 
-app.listen(process.env.PORT || 8080)
-
+app.listen(process.env.PORT || 8080);
