93466961a08b483c464a6c71fbc3fe350fd0bbd0
jack6177733651 <40873770+jack6177733651@users.noreply.github.com>
2020-12-14 16:54:16 -0500

    Change to heroku config

--- server.js
+++ server.js
@@ -6,41 +6,41 @@ const session = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
 const mongoose = require("mongoose");
-// mongoose.connect(
-//   `mongodb+srv://${process.env.DB_ADMIN_NAME}:${process.env.DB_PASSWORD}@cluster0.ecgcm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-//   { useNewUrlParser: true, useUnifiedTopology: true }
-// );
-
 mongoose.connect(
-  `mongoose.connect('mongodb://localhost:27017/cs5610-backend-db`,
+  `mongodb+srv://${process.env.DB_ADMIN_NAME}:${process.env.DB_PASSWORD}@cluster0.ecgcm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
+
+// mongoose.connect(
+//   `mongoose.connect('mongodb://localhost:27017/cs5610-backend-db`,
+//   { useNewUrlParser: true, useUnifiedTopology: true }
+// );
 const db = mongoose.connection;
 
-// app.enable("trust proxy");
-// app.use(
-//   session({
-//     resave: false, // don't save session if unmodified
-//     saveUninitialized: false, // don't create session until something stored
-//     secret: "adopifjqeporihgepoih349ru834tgihej",
-//     store: new MongoStore({ mongooseConnection: db }),
-//     proxy: true,
-//     cookie: {
-//       secure: true,
-//       maxAge: 3600000,
-//       sameSite: "None",
-//     },
-//   })
-// );
-
+app.enable("trust proxy");
 app.use(
   session({
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
     secret: "adopifjqeporihgepoih349ru834tgihej",
     store: new MongoStore({ mongooseConnection: db }),
+    proxy: true,
+    cookie: {
+      secure: true,
+      maxAge: 3600000,
+      sameSite: "None",
+    },
   })
 );
+
+// app.use(
+//   session({
+//     resave: false, // don't save session if unmodified
+//     saveUninitialized: false, // don't create session until something stored
+//     secret: "adopifjqeporihgepoih349ru834tgihej",
+//     store: new MongoStore({ mongooseConnection: db }),
+//   })
+// );
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -48,8 +48,8 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    // origin: "https://tranquil-basin-87439.herokuapp.com",
+    // origin: "http://localhost:3000",
+    origin: "https://tranquil-basin-87439.herokuapp.com",
     credentials: true,
   })
 );
